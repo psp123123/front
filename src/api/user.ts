9befb4e7c8e71eab05e15f4016ad97af199aa5c7
潘少鹏 <v-panshaopeng@xiaomi.com>
2025-10-15 10:07:59 +0800

    test

--- src/api/user.ts
+++ src/api/user.ts
@@ -59,12 +59,11 @@ interface LoginResponse {
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: ,
+  baseURL: import.meta.env.BASE_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
-  
 })
 
 console.log('拼接的请求url:', import.meta.env.BASE_URL)
