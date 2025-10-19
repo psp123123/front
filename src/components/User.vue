3cfe3d7a552915164b9c3f4ccd9cf1374deabba0
潘少鹏 <v-panshaopeng@xiaomi.com>
2025-10-19 17:40:24 +0800

    add func

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <el-col :span="8" class="user-menu">
+  <el-col :span="8">
     <el-dropdown trigger="click">
       <span class="el-dropdown-link">
         <img src="/avatar.png" class="avatar-image" alt="avatar" />
@@ -98,90 +98,17 @@ const logout = (): void => {
   router.push('/logout')
 }
 </script>
+
 <style scoped>
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.user-name {
-  cursor: pointer;
-  padding: 8px 12px 0;
-  border: 0;
-  border-radius: 4px;
-  background-color: rgba(45, 44, 13, 0);
-  /* background-color: rgba(255, 255, 255, 0); */
-  /* user-select: none; */
-  width: 100px;
-  border: 1px solid #ccc;
-}
-
-/* 下拉菜单样式 */
-.user-select-dropdown {
-  position: absolute;
-  top: 100%;
-  right: 0;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  /* padding: 8px 12px; */
-  background: white;
-  border: 1px solid #ddd;
-  border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  width: 100%;
-  z-index: 1000;
-  overflow: hidden;
-}
-
-.user-select-dropdown li {
-  padding: 10px 12px;
-  cursor: pointer;
+.block-col-2 .demonstration {
+  display: block;
+  color: var(--el-text-color-secondary);
   font-size: 14px;
+  margin-bottom: 20px;
 }
 
-.user-select-dropdown li:hover {
-  background-color: #f0f0f0;
-}
-
-.user-select-dropdown li:not(:last-child) {
-  border-bottom: 1px solid #eee;
-}
-
-.avatar-image {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-}
-
-:deep(.user-menu .el-dropdown-link) {
-  display: inline-flex !important;
+.block-col-2 .el-dropdown-link {
+  display: flex;
   align-items: center;
-  justify-content: center;
-  gap: 6px;
-  /* 用户名与箭头的间距 */
-  font-size: 18px;
-  /* 调整整体字体大小 */
-  font-weight: 500;
-  line-height: 1;
-  /* 避免换行 */
-  cursor: pointer;
-  color: var(--el-text-color-primary);
-  vertical-align: middle;
-  /* 保持整体垂直居中 */
-}
-
-:deep(.user-menu .el-dropdown-link .el-icon) {
-  font-size: 16px;
-  /* 图标大小略小一点 */
-  line-height: 1;
-  display: inline-flex;
-  align-items: center;
-}
-
-:deep(.avatar-image) {
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
 }
 </style>
